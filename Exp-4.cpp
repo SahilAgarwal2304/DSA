f9dff574e8100e1df5f2a1076b28b71d4fd3f612
SahilAgarwal2304 <166193356+SahilAgarwal2304@users.noreply.github.com>
2025-01-09 20:37:53 +0530

    Update Exp-4.cpp
    Final

--- Exp-4.cpp
+++ Exp-4.cpp
@@ -69,39 +69,156 @@ int countElements(const vector<int>& arr) {
     return count;
 }
 
+vector<int> intersection(const vector<int>& arr1, const vector<int>& arr2) {
+    vector<int> result;
+    for (int i = 0; i < arr1.size(); i++) {
+        for (int j = 0; j < arr2.size(); j++) {
+            if (arr1[i] == arr2[j]) {
+                result.push_back(arr1[i]);
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+vector<int> unionSets(const vector<int>& arr1, const vector<int>& arr2) {
+    vector<int> result = arr1;
+    for (int i = 0; i < arr2.size(); i++) {
+        bool found = false;
+        for (int j = 0; j < result.size(); j++) {
+            if (arr2[i] == result[j]) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            result.push_back(arr2[i]);
+        }
+    }
+    return result;
+}
+
+vector<int> difference1(const vector<int>& arr1, const vector<int>& arr2) {
+    vector<int> result;
+    for (int i = 0; i < arr1.size(); i++) {
+        bool found = false;
+        for (int j = 0; j < arr2.size(); j++) {
+            if (arr1[i] == arr2[j]) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            result.push_back(arr1[i]);
+        }
+    }
+    return result;
+}
+
+vector<int> difference2(const vector<int>& arr1, const vector<int>& arr2) {
+    vector<int> result;
+    for (int i = 0; i < arr2.size(); i++) {
+        bool found = false;
+        for (int j = 0; j < arr1.size(); j++) {
+            if (arr2[i] == arr1[j]) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            result.push_back(arr2[i]);
+        }
+    }
+    return result;
+}
+
+bool isSubset(const vector<int>& arr1, const vector<int>& arr2) {
+    for (int i = 0; i < arr1.size(); i++) {
+        bool found = false;
+        for (int j = 0; j < arr2.size(); j++) {
+            if (arr1[i] == arr2[j]) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
-    vector<int> arr;
-    accept(arr);
+    vector<int> arr1, arr2;
     char flag = 'y';
     while (flag == 'y' || flag == 'Y') {
-        cout << "Current array: ";
-        printArray(arr);
+        cout << "Current array 1: ";
+        printArray(arr1);
         cout << "Choose an operation: " << endl;
-        cout << "1. Remove an element" << endl;
-        cout << "2. Find an element" << endl;
-        cout << "3. Print array" << endl;
-        cout << "4. Count elements" << endl;
-        cout << "5. Exit" << endl;
+        cout << "1. Accept array 1" << endl;
+        cout << "2. Accept array 2" << endl;
+        cout << "3. Remove an element from array 1" << endl;
+        cout << "4. Remove an element from array 2" << endl;
+        cout << "5. Find an element in array 1" << endl;
+        cout << "6. Find an element in array 2" << endl;
+        cout << "7. Print array 1" << endl;
+        cout << "8. Print array 2" << endl;
+        cout << "9. Count elements in array 1" << endl;
+        cout << "10. Count elements in array 2" << endl;
+        cout << "11. Find intersection of array 1 and array 2" << endl;
+        cout << "12. Find union of array 1 and array 2" << endl;
+        cout << "13. Find difference of array 1 from array 2" << endl;
+        cout << "14. Find difference of array 2 from array 1" << endl;
+        cout << "15. Check if array 1 is a subset of array 2" << endl;
+        cout << "16. Exit" << endl;
         int choice;
         cin >> choice;
         switch (choice) {
-            case 1:
-                remove(arr);
+            case 1: accept(arr1); break;
+            case 2: accept(arr2); break;
+            case 3: remove(arr1); break;
+            case 4: remove(arr2); break;
+            case 5: findElement(arr1); break;
+            case 6: findElement(arr2); break;
+            case 7: printArray(arr1); break;
+            case 8: printArray(arr2); break;
+            case 9: cout << "Number of elements in array 1: " << countElements(arr1) << endl; break;
+            case 10: cout << "Number of elements in array 2: " << countElements(arr2) << endl; break;
+            case 11: {
+                vector<int> intersectionResult = intersection(arr1, arr2);
+                cout << "Intersection of array 1 and array 2: ";
+                printArray(intersectionResult);
                 break;
-            case 2:
-                findElement(arr);
+            }
+            case 12: {
+                vector<int> unionResult = unionSets(arr1, arr2);
+                cout << "Union of array 1 and array 2: ";
+                printArray(unionResult);
                 break;
-            case 3:
-                printArray(arr);
+            }
+            case 13: {
+                vector<int> differenceResult1 = difference1(arr1, arr2);
+                cout << "Difference of array 1 from array 2: ";
+                printArray(differenceResult1);
                 break;
-            case 4:
-                cout << "Number of elements in the array: " << countElements(arr) << endl;
+            }
+            case 14: {
+                vector<int> differenceResult2 = difference2(arr1, arr2);
+                cout << "Difference of array 2 from array 1: ";
+                printArray(differenceResult2);
                 break;
-            case 5:
-                flag = 'n';
-                continue;
-            default:
-                cout << "Invalid choice! Please try again." << endl;
+            }
+            case 15: {
+                if (isSubset(arr1, arr2)) {
+                    cout << "Array 1 is a subset of Array 2." << endl;
+                } else {
+                    cout << "Array 1 is NOT a subset of Array 2." << endl;
+                }
+                break;
+            }
+            case 16: flag = 'n'; continue;
+            default: cout << "Invalid choice! Please try again." << endl;
         }
         if (flag != 'n') {
             cout << "Do you want to continue? (y/n): ";
